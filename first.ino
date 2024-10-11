b8e56e2a1aa41e75590593b2c703369c3a4fdbdb
Amin <155407117+Amin-prog5@users.noreply.github.com>
2024-10-11 11:31:54 +0300

    Traffic light.ino
    Used in this project 3 LEDS red (pin 7) , Yellow (pin 12) and green(pin 8) and 3 resistance (1k) all with Arduino  micro Controller

---
+++ first.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  digitalWrite(7,HIGH);
+  delay(3000);
+  digitalWrite(7,LOW);
+  delay(5);
+
+
+  
+  digitalWrite(12,HIGH);
+  delay(1500);
+  digitalWrite(12,LOW);
+  delay(10);
+
+  digitalWrite(8,HIGH);
+  delay(3000);
+  digitalWrite(8,LOW);
+  delay(15);
+
+
+  digitalWrite(12,HIGH);
+  delay(1500);
+  digitalWrite(12,LOW);
+  delay(20);
+
+}
